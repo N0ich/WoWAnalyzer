7463edcd85e4bda41bb9308a26d6b3f12791c4bf
Noich <vin100.rey@gmail.com>
2017-11-13 16:48:26 +0100

    [ADD] Added a few more spells as well as generic cast efficiency values

--- src/Parser/Paladin/Protection/Modules/Features/CastEfficiency.js
+++ src/Parser/Paladin/Protection/Modules/Features/CastEfficiency.js
@@ -17,7 +17,12 @@ class CastEfficiency extends CoreCastEfficiency {
       getCooldown: haste => 9 / (1 + haste),
       recommendedCastEfficiency: 0.9,
 	},
-	  // work on CD
+    {
+      spell: SPELLS.JUDGMENT_CAST,
+      category: CastEfficiency.SPELL_CATEGORIES.ROTATIONAL,
+      getCooldown: haste => 12 / (1 + haste),
+      recommendedCastEfficiency: 0.9,
+	},
     {
       spell: SPELLS.BLESSED_HAMMER_TALENT,
       category: CastEfficiency.SPELL_CATEGORIES.ROTATIONAL,
@@ -35,7 +40,7 @@ class CastEfficiency extends CoreCastEfficiency {
       recommendedCastEfficiency: 0.6,
       importance: ISSUE_IMPORTANCE.MINOR,
 	},
-	  // Probably useless to try to count the number of casts
+	  // Will do nb of casts in another file
     {
       spell: SPELLS.SHIELD_OF_THE_RIGHTEOUS,
       category: CastEfficiency.SPELL_CATEGORIES.ROTATIONAL,
@@ -43,12 +48,67 @@ class CastEfficiency extends CoreCastEfficiency {
       charges: 3,
       recommendedCastEfficiency: 0.8,
 	},
+    {
+      spell: SPELLS.AVENGERS_SHIELD,
+      category: CastEfficiency.SPELL_CATEGORIES.ROTATIONAL,
+      getCooldown: haste => 15 / (1 + haste),
+      recommendedCastEfficiency: 0.9,
+	},
+
 	  //COOLDOWNS
     {
       spell: SPELLS.EYE_OF_TYR,
       category: CastEfficiency.SPELL_CATEGORIES.COOLDOWNS,
       getCooldown: (haste, combatant) => (combatant.hasShoulder(ITEMS.PILLARS_OF_INMOST_LIGHT.id) ? 45 : 60),
       recommendedCastEfficiency: 0.85,
+	},
+    {
+      spell: SPELLS.AEGIS_OF_LIGHT_TALENT,
+      category: CastEfficiency.SPELL_CATEGORIES.COOLDOWNS,
+      getCooldown: haste => 180,
+      isActive: combatant => combatant.hasTalent(SPELLS.AEGIS_OF_LIGHT_TALENT.id),
+      recommendedCastEfficiency: 0.5,
+	},
+	  // calculate CDR based on forbearance
+    {
+      spell: SPELLS.BLESSING_OF_SPELLWARDING_TALENT,
+      category: CastEfficiency.SPELL_CATEGORIES.COOLDOWNS,
+      isActive: combatant => combatant.hasTalent(SPELLS.BLESSING_OF_SPELLWARDING_TALENT.id),
+      getCooldown: haste => 180,
+	},
+	  // calculate CDR based on forbearance
+    {
+      spell: SPELLS.BLESSING_OF_PROTECTION,
+      category: CastEfficiency.SPELL_CATEGORIES.COOLDOWNS,
+      isActive: combatant => !combatant.hasTalent(SPELLS.BLESSING_OF_SPELLWARDING_TALENT.id),
+      getCooldown: haste => 300,
+	},
+    {
+      spell: SPELLS.BASTION_OF_LIGHT_TALENT,
+      category: CastEfficiency.SPELL_CATEGORIES.COOLDOWNS,
+      getCooldown: haste => 120,
+      isActive: combatant => combatant.hasTalent(SPELLS.BASTION_OF_LIGHT_TALENT.id),
+      recommendedCastEfficiency: 0.9,
+	},
+	  // Calculate CDR based on weapon
+    {
+      spell: SPELLS.ARDENT_DEFENDER,
+      category: CastEfficiency.SPELL_CATEGORIES.COOLDOWNS,
+      getCooldown: haste => null,
+      recommendedCastEfficiency: 0.85,
+	},
+    {
+      spell: SPELLS.SERAPHIM_TALENT,
+      category: CastEfficiency.SPELL_CATEGORIES.COOLDOWNS,
+      getCooldown: haste => 45,
+      isActive: combatant => combatant.hasTalent(SPELLS.SERAPHIM_TALENT.id),
+      recommendedCastEfficiency: 0.90,
+	},
+    {
+      spell: SPELLS.GUARDIAN_OF_ANCIENT_KINGS,
+      category: CastEfficiency.SPELL_CATEGORIES.COOLDOWNS,
+      getCooldown: haste => 300,
+      recommendedCastEfficiency: 0.70,
 	},
 
   ];
